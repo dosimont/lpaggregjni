cf954ffc8594ee086e0167feaa5a58ce6047c4d5
Damien Dosimont <damien.dosimont@imag.fr>
2014-02-04 17:22:01 +0100

    gitignore

--- Release/src/lpaggreg_wrap.d
+++
@@ -1,6 +0,0 @@
-src/lpaggreg_wrap.d: ../src/lpaggreg_wrap.cxx \
- /usr/java/latest/include/jni.h /usr/java/latest/include/linux/jni_md.h
-
-/usr/java/latest/include/jni.h:
-
-/usr/java/latest/include/linux/jni_md.h:
