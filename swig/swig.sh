324a1f1ed3de9345edf679f467e0823cdacaf094
Damien Dosimont <damien.dosimont@imag.fr>
2014-03-31 15:45:46 +0200

    swig

--- swig/swig.sh
+++ swig/swig.sh
@@ -5,12 +5,12 @@ LPAGGREG_SRC=$LPAGGREG/src
 LPAGGREGJNI_SRC=../src
 JAVA_BIN=../java
 
-mkdir -p $JAVA_BIN
-mkdir -p $LPAGGREGJNI_SRC
-mkdir temp
-swig -c++ -java -package $TARGET_PACKAGE -outdir temp lpaggreg.i
-cp temp/* $JAVA_BIN
-rm -fr temp
+mkdir -p $JAVA_BIN &&
+mkdir -p $LPAGGREGJNI_SRC &&
+mkdir temp &&
+swig -c++ -java -package $TARGET_PACKAGE -outdir temp lpaggreg.i &&
+cp temp/* $JAVA_BIN &&
+rm -fr temp &&
 mv lpaggreg_wrap.cxx $LPAGGREGJNI_SRC
 
 exit 0
